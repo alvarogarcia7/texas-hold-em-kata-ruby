05c31e3b105ea89e8263b1e0d744591b1ece11e5
Alvaro Garcia <derecerca@gmail.com>
2015-10-11 15:50:16 +0200

    red: read file contents

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -9,6 +9,15 @@ end
 RSpec.describe "#parser" do
   it "should read the file" do
   	read_values = Parser.new(File.dirname(__FILE__)+"/samples/hand1.hd")
-  	expect(read_values.lines).to eq 8 
+  	expect(read_values.lines).to eq [
+      "4s Kd 2h Ts 5h Jc",
+      "8h Ad 2h Ts 5h",
+      "2c 6s 2h Ts 5h",
+      "4d Tc 2h Ts 5h Jc",
+      "Jh 3d 2h Ts 5h Jc Qd",
+      "4h As",
+      "9d Jd",
+      "7s 4c 2h Ts 5h Jc"
+    ]
   end
 end
