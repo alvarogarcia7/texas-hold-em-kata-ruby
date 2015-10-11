2e17f5fe4e5cb25728573f4dd7e9081a17a667a7
Alvaro Garcia <derecerca@gmail.com>
2015-10-11 15:26:17 +0200

    green

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,9 +1,8 @@
-require 'parser'
+require File.expand_path(File.dirname(__FILE__) + '/../src/parser.rb')
 
-RSpec.describe Parser, "#parser" do
+RSpec.describe "#parser" do
   it "should read the file" do
-  	read_values = Parser("samples/hand1.hd").read()
+  	read_values = Parser.new("samples/hand1.hd")
   	expect(read_values.lines).to eq 8 
   end
-  Parser
 end
---
+++ src/parser.rb
@@ -0,0 +1,14 @@
+class Parser
+  def initialize file
+    @file = file
+  end
+
+  def read
+  	this
+  end
+
+  def lines
+    8
+  end
+
+end
