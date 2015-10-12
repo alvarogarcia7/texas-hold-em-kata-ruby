58817f8434e02e155f124ffd3590100d620fabe6
Alvaro Garcia <derecerca@gmail.com>
2015-10-12 02:07:19 +0200

    defect: was comparing expected vs expected result

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe 'Rule application' do
     initial_hand = Hand.from('4s 5s')
     expected_result = {used: Hand.from('5s'), kicker: Hand.from('4s')}
     actual_result = rule.apply(initial_hand)
-    expect(actual_result).to eq(actual_result)
+    expect(actual_result).to eq(expected_result)
   end
 end
 
