3d7641dafd7aafcf85f2fe98544618f4b56fd517
Alvaro Garcia <derecerca@gmail.com>
2015-10-18 13:23:27 +0200

    refactor: remove complexity using the last instead of the first access to the hand rank

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -171,15 +171,14 @@ class Spike1
   end
 
   def describe
-    sorted_rules = [Rule::THREE_OF_A_KIND, Rule::TWO_PAIR, Rule::PAIR, Rule::HIGH_CARD]
-    rules = sorted_rules.reverse
+    rules = [Rule::THREE_OF_A_KIND, Rule::TWO_PAIR, Rule::PAIR, Rule::HIGH_CARD]
 
     hand_with_type_with_rule_index = @hands.map{|hand|
       rules.map{|rule|
         [hand.apply(rule), rule]
       }.select{|x| not x.first[:used].empty?}.reverse
-    }.map{|x| x.first}
-     .map{|x| [*x, sorted_rules.index(x[1])]}
+    }.map{|x| x.last}
+     .map{|x| [*x, rules.index(x[1])]}
 
     # p hand_with_type_with_rule_index
     desc = @hands.each_with_index.map {|hand, index|
