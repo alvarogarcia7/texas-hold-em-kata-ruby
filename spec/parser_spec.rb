588a4cba3ef3d9e5ac165bc04f0d17e8e53a55db
Alvaro Garcia <derecerca@gmail.com>
2015-11-02 16:33:56 +0100

    refactor: extract class to its own file

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../src/parser.rb')
+require_relative '../src/hand_descriptor.rb'
 
 RSpec.describe 'Canary test' do
   it 'should always pass' do
@@ -161,44 +162,4 @@ describe "#General Play" do
 8h Ad 2h Ts 5h")
   end
 end
-class HandDescriptor
-  def self.describe(hands)
-    new(hands).describe
-  end
 
-  def initialize(hands)
-    @hands = hands
-  end
-
-  def describe
-    rules = [Rule::THREE_OF_A_KIND, Rule::TWO_PAIR, Rule::PAIR, Rule::HIGH_CARD]
-
-    applied_non_empty_rules = @hands.map { |hand|
-      rules.map { |rule|
-        [hand.apply(rule), rule]
-      }.select { |x| not x.first[:used].empty? }.reverse
-    }
-    most_valuable_rule = applied_non_empty_rules.map{|x| x.last}
-     .map{|x| [*x, rules.index(x[1])]}
-
-    # p hand_with_type_with_rule_index
-    desc = @hands.each_with_index.map {|hand, index|
-      rule = most_valuable_rule[index][1]
-      rule_name = if rule == Rule::HIGH_CARD then '' else " #{rule.name}" end
-      [hand.cards.map{|x| x.value}.join(' ') + rule_name, most_valuable_rule[index][2]]
-    }
-
-    winner_hand = most_valuable_rule.map{|x| x[2]}.min
-
-    desc.select{|x| x[1] == winner_hand}.first[0] += ' (winner)'
-
-    desc.map!{|x| x.first}
-    return desc.join "\n"
-  end
-
-  def apply_next(rule)
-    @hands.map { |hand| (hand.apply rule)[:used] }
-  end
-
-
-end
---
+++ src/hand_descriptor.rb
@@ -0,0 +1,43 @@
+class HandDescriptor
+  def self.describe(hands)
+    new(hands).describe
+  end
+
+  def initialize(hands)
+    @hands = hands
+  end
+
+  def describe
+    rules = [Rule::THREE_OF_A_KIND, Rule::TWO_PAIR, Rule::PAIR, Rule::HIGH_CARD]
+
+    applied_non_empty_rules = @hands.map { |hand|
+      rules.map { |rule|
+        [hand.apply(rule), rule]
+      }.select { |x| not x.first[:used].empty? }.reverse
+    }
+    most_valuable_rule = applied_non_empty_rules.map { |x| x.last }
+                             .map { |x| [*x, rules.index(x[1])] }
+
+    # p hand_with_type_with_rule_index
+    desc = @hands.each_with_index.map { |hand, index|
+      rule = most_valuable_rule[index][1]
+      rule_name = if rule == Rule::HIGH_CARD then
+                    ''
+                  else
+                    " #{rule.name}"
+                  end
+      [hand.cards.map { |x| x.value }.join(' ') + rule_name, most_valuable_rule[index][2]]
+    }
+
+    winner_hand = most_valuable_rule.map { |x| x[2] }.min
+
+    desc.select { |x| x[1] == winner_hand }.first[0] += ' (winner)'
+
+    desc.map! { |x| x.first }
+    return desc.join "\n"
+  end
+
+  def apply_next(rule)
+    @hands.map { |hand| (hand.apply rule)[:used] }
+  end
+end
