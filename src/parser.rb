dc7f0b2e9d086d0f51587981b44a19c1d259d300
Alvaro Garcia <derecerca@gmail.com>
2015-11-13 13:02:22 +0100

    refactor: use idiomatic sort rather than 3 HOFs

--- src/parser.rb
+++ src/parser.rb
@@ -163,9 +163,7 @@ class Rule
   HIGH_CARD = Rule.new(
       lambda { |cards|
         sorted_cards = cards
-                           .map { |x| [x, Card::VALID_RANKS.index(x.rank)]}
-                           .sort_by { |f| f[1]}
-                           .map { |x| x.first}
+                           .sort_by { |x| Card::VALID_RANKS.index(x.rank)}
         used = sorted_cards.first
         kicker = sorted_cards[1]
         if kicker.nil? then
